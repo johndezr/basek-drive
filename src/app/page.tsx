39cfde250ceeff8c8873326144b6ec8d55bb28b9
John <info@jgalindez.net>
2025-03-13 14:41:11 +0100

    Add utility functions and button component; update layout and dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -47,6 +48,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button>Button</Button>
         </div>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
